fe3e503a5b2723f2030531296ae4c1014044999e
zhoulin <zhoulin.233@bytedance.com>
2022-10-12 10:52:40 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'import', 'eslint-plugin-prettier'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'max-lines': [
+      'error',
+      {
+        max: 350,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'import/no-named-as-default': 'off',
+    'import/default': 'off',
+    'import/order': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'no-console': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+  },
+};
---
+++ README.md
@@ -0,0 +1,47 @@
+# wakuwaku
+
+<img src='./wakuwaku.png' width='50%'/>
+
+## 项目研发
+
+### 本地研发
+
+:sparkles: 切换到 `dev` 分支开发
+
+1. 配置淘宝镜像源
+
+```bash
+npm config set registry https://registry.npm.taobao.org
+```
+
+2. 初始化项目
+
+```bash
+git clone https://github.com/regaliastar/wakuwaku.git
+cd wakuwaku
+npm install
+```
+
+3. 运行
+```bash
+npm run start
+```
+
+4. 重新编译
+```bash
+npm run watch
+```
+
+### 目录结构
+
+- scripts --> 工程化支撑脚本，包括 webpack 配置等。
+- shell --> 构建脚本。
+- src --> 项目源码目录
+  - entry 项目的入口，包括整体布局、路由和页面菜单。
+  - page 具体的页面模块。
+  - common 公共组件
+    - interface 接口类型统一放在这个目录下。
+    - component 业务组件。
+    - less 全局统一的样式。
+    - util 全局的工具类。
+    - store 存储。
